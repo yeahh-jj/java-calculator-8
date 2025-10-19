1e607e52d442a46d22cea6b8abe573e7e32eeaa0
yeahh-jj <jyll1009@gmail.com>
2025-10-20 05:13:17 +0900

    feat: add empty string check

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,11 +1,17 @@
 package calculator.controller;
 
+import calculator.model.StringCalculator;
 import calculator.view.View;
 
 public class CalculatorController {
     private View view = new View();
+    private StringCalculator stringCalculator = new StringCalculator();
 
     public void run() {
         String input = view.inputValue();
+        int result = stringCalculator.calculate(input);
+        view.outValue(result);
     }
+
+
 }
--- src/main/java/calculator/model/StringCalculator.java
+++ src/main/java/calculator/model/StringCalculator.java
@@ -2,4 +2,16 @@ package calculator.model;
 
 public class StringCalculator {
 
+    public int calculate(String inputValue) {
+        if(inputValue == null || inputValue.isBlank() ) {
+            return 0;
+        }else {
+            return calculateExpression(inputValue);
+        }
+    }
+
+    private int calculateExpression(String inputValue) {
+        int result = 1; // 임시
+        return result;
+    }
 }
