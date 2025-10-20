a7100d0f73fa0ba4aa9c6c2b0247e16b6a119c59
yeahh-jj <jyll1009@gmail.com>
2025-10-20 18:37:27 +0900

    feat: add custom delimiter feature and handle '\n' input from console

--- src/main/java/calculator/model/StringCalculator.java
+++ src/main/java/calculator/model/StringCalculator.java
@@ -9,19 +9,36 @@ public class StringCalculator {
             return 0;
         }
       
-        String[] numbers = splitWithDefaultDelimiter(inputValue);
+        String[] numbers = splitNumbers(inputValue);
         return sum(numbers);
     }
 
-    private int sum(String[] numbers) {
-        int total = 0;
-        return total;
-    }
+    private String[] splitNumbers(String inputValue) {
+        inputValue = inputValue.replace("\\n", "\n");
 
-    public String[] splitWithDefaultDelimiter(String inputValue) {
+        if(inputValue.startsWith("//")) {
+            return splitWithCustomDelimiter(inputValue);
+        }
+
+        return splitWithDefaultDelimiter(inputValue);
+    }
+    
+    private String[] splitWithDefaultDelimiter(String inputValue) {
         String defaultDelimiter = ",|:";
         // System.out.println(Arrays.toString(inputValue.split(defaultDelimiter)));
         return inputValue.split(defaultDelimiter);
     }
+    
+    private String[] splitWithCustomDelimiter(String inputValue) {
+        int delimiterEndIdx = inputValue.indexOf("\n");
+        String customDelimiter = inputValue.substring(2, delimiterEndIdx);
+        String numbersPart = inputValue.substring(delimiterEndIdx + 1);
 
+        return numbersPart.split(customDelimiter);
+    }
+    
+    private int sum(String[] numbers) {
+        int total = 0;
+        return total;
+    }
 }
